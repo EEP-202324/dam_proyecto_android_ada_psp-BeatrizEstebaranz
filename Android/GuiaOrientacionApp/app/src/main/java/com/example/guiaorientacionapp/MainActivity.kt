2f543967331679bb886b3d577be6e05e127b1a98
BeatrizEstebaranz <145351140+BeatrizEstebaranz@users.noreply.github.com>
2024-05-13 09:11:07 +0200

    Creación primera pantalla en android con  impllementación a mejorar

--- Android/GuiaOrientacionApp/app/src/main/java/com/example/guiaorientacionapp/MainActivity.kt
+++ Android/GuiaOrientacionApp/app/src/main/java/com/example/guiaorientacionapp/MainActivity.kt
@@ -18,8 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.guiaorientacion.model.Actividad
-import com.example.guiaorientacion.data.ActividadDataSource
 import com.example.guiaorientacionapp.api.ApiService
 import com.example.guiaorientacionapp.model.Actividad
 import retrofit2.Response
@@ -33,4 +31,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-}
+
--- Android/GuiaOrientacionApp/app/src/main/java/com/example/guiaorientacionapp/theme/PantallaInicio.kt
+++ Android/GuiaOrientacionApp/app/src/main/java/com/example/guiaorientacionapp/theme/PantallaInicio.kt
@@ -1,2 +1,96 @@
 package com.example.guiaorientacionapp.theme
 
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.guiaorientacionapp.R
+
+/**
+ * Composable that allows the user to select the desired cupcake quantity and expects
+ * [onNextButtonClicked] lambda that expects the selected quantity and triggers the navigation to
+ * next screen
+ */
+@Composable
+fun PantallaInicio(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.width(300.dp)
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+            Text(
+                text = stringResource(R.string.futuro),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(
+                dimensionResource(id = R.dimen.padding_medium)
+            )
+        ) {
+            BotonInicio(
+                labelResourceId = R.string.enter,
+                onClick = {}
+            )
+        }
+    }
+}
+
+/**
+ * Botón personalizado que muestra el [labelResourceId]
+ * y activa la lambda [onClick] cuando se hace clic en este componible
+ */
+@Composable
+fun BotonInicio(
+    @StringRes labelResourceId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+     Button(
+        onClick = onClick,
+        modifier = modifier.widthIn(min = 250.dp)
+    ) {
+        Text(stringResource(labelResourceId))
+    }
+}
+
+@Preview
+@Composable
+fun PantallaInicioPreview() {
+    PantallaInicio()
+}
