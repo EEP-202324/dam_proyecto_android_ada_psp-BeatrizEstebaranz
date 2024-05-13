5f32ffe56f1ad6e57cee4ed6231eca71df63aa02
BeatrizEstebaranz <145351140+BeatrizEstebaranz@users.noreply.github.com>
2024-05-13 10:15:40 +0200

    implementacion clase listado en proeso

--- Android/GuiaOrientacionApp/app/build.gradle.kts
+++ Android/GuiaOrientacionApp/app/build.gradle.kts
@@ -67,6 +67,10 @@ dependencies {
     implementation ("androidx.compose.material:material:1.0.5")
     implementation ("androidx.activity:activity-compose:1.3.1")
 
+    implementation("com.squareup.retrofit2:converter-gson:2.7.2")
+
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
