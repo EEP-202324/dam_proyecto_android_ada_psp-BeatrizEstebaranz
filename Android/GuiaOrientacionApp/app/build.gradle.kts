b5de6985e888481b2759302320b9625f84058362
BeatrizEstebaranz <145351140+BeatrizEstebaranz@users.noreply.github.com>
2024-05-14 13:12:36 +0200

    error conseguido

--- Android/GuiaOrientacionApp/app/build.gradle.kts
+++ Android/GuiaOrientacionApp/app/build.gradle.kts
@@ -74,6 +74,14 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+        implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Android/GuiaOrientacionApp/app/src/main/java/com/example/guiaorientacionapp/api/ApiService.kt
+++ Android/GuiaOrientacionApp/app/src/main/java/com/example/guiaorientacionapp/api/ApiService.kt
@@ -3,17 +3,19 @@ package com.example.guiaorientacionapp.api
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.Retrofit
-
 import retrofit2.http.GET
 import com.example.guiaorientacionapp.model.Actividad
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import retrofit2.http.Url
 import kotlinx.serialization.json.Json
 
+import retrofit2.converter.gson.GsonConverterFactory
+
+
     private const val BASE_URL = "https://10.0.2.2:8080"
 
     private val retrofit = Retrofit.Builder()
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .addConverterFactory(GsonConverterFactory.create())
         .baseUrl(BASE_URL)
         .build()
 
